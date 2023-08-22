0ee54fef9cab98bd57cd4838dfa0f91b81e30ab2
chetan <chetankumar70174577@gmail.com>
2023-08-22 09:48:02 +0530

    adding animation in website

---
+++ main.js
@@ -0,0 +1,31 @@
+// these are for  animation 
+
+AOS.init();
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 900, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+
+// diappearing email field initially 
+let email_para = document.getElementById("emailpara");
+email_para.style.display = "none";
+// appear email field when checked
+let checkbox = document.getElementById("subscribe");
+checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+        email_para.style.display = "block";
+    } else {
+        // alert("not checked");
+    }
+});
